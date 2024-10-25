ee38c86eb984c609ec0e89df88f263e8abc401d8
manuelk <manuelk@IT-W0004D>
2024-10-25 10:20:14 +0800

    Add project files.

---
+++ Calculator/Form1.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace Calculator
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnCompute = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label6 = new System.Windows.Forms.Label();
+            this.amountPurchase = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.amountTendered = new System.Windows.Forms.TextBox();
+            this.listOption = new System.Windows.Forms.ComboBox();
+            this.computeBox = new System.Windows.Forms.RichTextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.CalculatedSum = new System.Windows.Forms.TextBox();
+            this.computedBox2 = new System.Windows.Forms.RichTextBox();
+            this.panel1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Font = new System.Drawing.Font("Courier New", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(393, 54);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Discount Calculator";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Courier New", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label2.Location = new System.Drawing.Point(148, 268);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 27);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Output";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(3, 211);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(168, 18);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Discount Options";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnCompute
+            // 
+            this.btnCompute.AutoSize = true;
+            this.btnCompute.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnCompute.Font = new System.Drawing.Font("Courier New", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCompute.Location = new System.Drawing.Point(315, 271);
+            this.btnCompute.Name = "btnCompute";
+            this.btnCompute.Size = new System.Drawing.Size(66, 25);
+            this.btnCompute.TabIndex = 8;
+            this.btnCompute.Text = "Compute";
+            this.btnCompute.UseVisualStyleBackColor = true;
+            this.btnCompute.Click += new System.EventHandler(this.SetArgs);
+            // 
+            // panel1
+            // 
+            this.panel1.AutoSize = true;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(393, 54);
+            this.panel1.TabIndex = 11;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.label6, 0, 7);
+            this.tableLayoutPanel1.Controls.Add(this.amountPurchase, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.label5, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label4, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.amountTendered, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 54);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 8;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(393, 128);
+            this.tableLayoutPanel1.TabIndex = 12;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(3, 106);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(148, 18);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Calculated Sum";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // amountPurchase
+            // 
+            this.amountPurchase.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.amountPurchase.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.amountPurchase.ForeColor = System.Drawing.Color.Gray;
+            this.amountPurchase.Location = new System.Drawing.Point(3, 77);
+            this.amountPurchase.Name = "amountPurchase";
+            this.amountPurchase.Size = new System.Drawing.Size(387, 26);
+            this.amountPurchase.TabIndex = 4;
+            this.amountPurchase.Text = "Enter number";
+            this.amountPurchase.Click += new System.EventHandler(this.SetArgs);
+            this.amountPurchase.Leave += new System.EventHandler(this.FocusLeave);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Dock = System.Windows.Forms.DockStyle.Left;
+            this.label5.Font = new System.Drawing.Font("Courier New", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(3, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(175, 21);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Tendered Amount";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Dock = System.Windows.Forms.DockStyle.Left;
+            this.label4.Font = new System.Drawing.Font("Courier New", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(3, 53);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(241, 21);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Total Amount Purchase";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // amountTendered
+            // 
+            this.amountTendered.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.amountTendered.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.amountTendered.ForeColor = System.Drawing.Color.Gray;
+            this.amountTendered.Location = new System.Drawing.Point(3, 24);
+            this.amountTendered.Name = "amountTendered";
+            this.amountTendered.Size = new System.Drawing.Size(387, 26);
+            this.amountTendered.TabIndex = 2;
+            this.amountTendered.Text = "Enter number";
+            this.amountTendered.Click += new System.EventHandler(this.SetArgs);
+            this.amountTendered.Leave += new System.EventHandler(this.FocusLeave);
+            // 
+            // listOption
+            // 
+            this.listOption.DisplayMember = "Solo Parent";
+            this.listOption.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listOption.FormattingEnabled = true;
+            this.listOption.Location = new System.Drawing.Point(3, 232);
+            this.listOption.Name = "listOption";
+            this.listOption.Size = new System.Drawing.Size(387, 24);
+            this.listOption.TabIndex = 6;
+            this.listOption.Click += new System.EventHandler(this.SetArgs);
+            // 
+            // computeBox
+            // 
+            this.computeBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.computeBox.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.computeBox.Location = new System.Drawing.Point(169, 302);
+            this.computeBox.Name = "computeBox";
+            this.computeBox.ReadOnly = true;
+            this.computeBox.Size = new System.Drawing.Size(224, 255);
+            this.computeBox.TabIndex = 9;
+            this.computeBox.Text = "";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // CalculatedSum
+            // 
+            this.CalculatedSum.Location = new System.Drawing.Point(3, 189);
+            this.CalculatedSum.Name = "CalculatedSum";
+            this.CalculatedSum.ReadOnly = true;
+            this.CalculatedSum.Size = new System.Drawing.Size(387, 20);
+            this.CalculatedSum.TabIndex = 14;
+            // 
+            // computedBox2
+            // 
+            this.computedBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.computedBox2.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.computedBox2.Location = new System.Drawing.Point(0, 302);
+            this.computedBox2.Name = "computedBox2";
+            this.computedBox2.ReadOnly = true;
+            this.computedBox2.Size = new System.Drawing.Size(171, 255);
+            this.computedBox2.TabIndex = 9;
+            this.computedBox2.Text = "";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(393, 569);
+            this.Controls.Add(this.computedBox2);
+            this.Controls.Add(this.CalculatedSum);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.computeBox);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnCompute);
+            this.Controls.Add(this.listOption);
+            this.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Lawson Philippines Inc.";
+            this.Load += new System.EventHandler(this.SetArgs);
+            this.panel1.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnCompute;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.ComboBox listOption;
+        private System.Windows.Forms.TextBox amountPurchase;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox amountTendered;
+        private System.Windows.Forms.RichTextBox computeBox;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox CalculatedSum;
+        private System.Windows.Forms.RichTextBox computedBox2;
+    }
+}
+
---
+++ Calculator/Form1.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            // Add TextChanged event handler for amountPurchase
+            amountPurchase.TextChanged += AmountPurchase_TextChanged;
+        }
+
+        private void AmountPurchase_TextChanged(object sender, EventArgs e)
+        {
+            UpdateCalculatedSum();
+        }
+
+        private void SetArgs(Object s, EventArgs e)
+        {
+            if (s is Button button)
+            {
+                switch (button.Name)
+                {
+                    case nameof(btnCompute):
+                        ComputeButton();
+                        break;
+                    // Add more cases for additional Buttons
+                    default:
+                        break;
+                }
+            }
+            else if (s is Form form)
+            {
+                string[] list = { "Solo Parent / 10%", "Athlete / 20%" };
+                foreach (string item in list)
+                {
+                    listOption.Items.Add(item);
+                }
+                listOption.SelectedIndex = 0;
+            }
+            else if (s is TextBox textBox)
+            {
+                if (textBox == amountPurchase)
+                {
+                    // No need to call UpdateCalculatedSum here anymore
+                }
+                if (textBox.Text.Equals("Enter number"))
+                {
+                    textBox.Text = "";
+                    textBox.ForeColor = Color.Black;
+                }
+            }
+            else if (s is ComboBox comboBox)
+            {
+                comboBox.DroppedDown = true;
+            }
+        }
+
+        private void FocusLeave(object s, EventArgs e)
+        {
+            if (s is TextBox textBox)
+            {
+                if (string.IsNullOrWhiteSpace(textBox.Text))
+                {
+                    textBox.Text = "Enter number";
+                    textBox.ForeColor = Color.Gray;
+                }
+            }
+        }
+
+        private void UpdateCalculatedSum()
+        {
+            try
+            {
+                // Split the input text by '+' and sum the values
+                string[] values = amountPurchase.Text.Split('+');
+                double total = 0;
+                foreach (string value in values)
+                {
+                    if (double.TryParse(value.Trim(), out double number))
+                    {
+                        total += number;
+                    }
+                }
+                CalculatedSum.Text = total.ToString("F2");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error calculating sum: {ex.Message}");
+            }
+        }
+
+        private void ComputeButton()
+        {
+            try
+            {
+                if (double.TryParse(CalculatedSum.Text, out double originalValue))
+                {
+                    double discountedValue = originalValue;           
+                    double LessVAT = 0;
+                    double finalAmount = 0;
+                    double AddLessVat = 0;
+                    double LessWHoldingTAX = 0;
+                    double TotalAmount = 0;
+                    if (listOption.SelectedItem != null)
+                    {
+                        string selectedItem = listOption.SelectedItem.ToString().Trim();
+
+                        if (selectedItem == "Solo Parent / 10%")
+                        {
+                            LessVAT = originalValue - (originalValue / 1.12);
+                            discountedValue = originalValue / 1.12 * 0.1;
+                            AddLessVat = 0;
+                            LessWHoldingTAX = 0;
+                            TotalAmount = originalValue / 1.12 - discountedValue;
+                        }
+                        else if (selectedItem == "Athlete / 20%")
+                        {
+                            LessVAT = originalValue - (originalValue / 1.12);
+                            discountedValue = originalValue / 1.12 * 0.2;
+                            AddLessVat = originalValue - (originalValue / 1.12);
+                            LessWHoldingTAX = 0;
+                            TotalAmount = (originalValue / 1.12 + AddLessVat) - discountedValue;
+                        }
+                    }
+
+                    if (double.TryParse(amountTendered.Text, out double tenderedAmount))
+                    {
+                        double change = tenderedAmount - finalAmount;
+
+                        if (listOption.SelectedIndex >= 0)
+                        {
+                            // Update computeBox
+                            computeBox.Text = FormatForComputeBox(
+                                originalValue,
+                                LessVAT,
+                                discountedValue,
+                                AddLessVat,
+                                LessWHoldingTAX,
+                                TotalAmount,
+                                change);
+
+                            // Update computedBox2
+                            computedBox2.Text = FormatForComputedBox2();
+                        }
+                        else
+                        {
+                            SetBox(Color.Black, "Error; Select discount from list");
+                        }
+                    }
+                    else
+                    {
+                        SetBox(Color.Black, "Error; Enter tendered amount");
+                    }
+                }
+                else
+                {
+                    SetBox(Color.Black, "Error; Enter total amount purchase");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}");
+            }
+        }
+
+        private void SetBox(Color color, string text)
+        {
+            computeBox.Clear();
+            computeBox.ForeColor = color;
+            computeBox.Text = text;
+        }
+
+        private string FormatForComputeBox(double originalValue, double LessVAT, double discountedValue, double AddLessVat, double LessWHoldingTAX, double TotalAmount, double change)
+        {
+            computeBox.ForeColor = Color.Black;
+           
+            const int width = 10; // Adjust the width as needed for alignment
+
+            // Create the receipt lines with right alignment
+            return new StringBuilder()
+                .AppendLine($"Total Sales: {originalValue:F2}".PadLeft(width))
+                .AppendLine($"Less VAT: {LessVAT:F2}".PadLeft(width))
+                .AppendLine($"Amount Net of VAT: {originalValue / 1.12:F2}".PadLeft(width))
+                .AppendLine($"Less Discount: {discountedValue:F2}".PadLeft(width))
+                .AppendLine($"Add: VAT: {AddLessVat:F2}".PadLeft(width))
+                .AppendLine($"Less withholding Tax: {LessWHoldingTAX:F2}".PadLeft(width))
+                .AppendLine($"Total Amount: {TotalAmount:F2}".PadLeft(width))
+                .AppendLine($"Change: {change - TotalAmount:F2}".PadLeft(width))
+                .ToString();
+        }
+
+        private string FormatForComputedBox2()
+        {
+            computedBox2.ForeColor = Color.Black;
+            const int valueWidth = 10; // Adjust this width based on your needs
+
+            return new StringBuilder()
+                .AppendLine($"Vatable Sales: {0.00:F2}".PadLeft(valueWidth))
+                .AppendLine($"Vat:   {0.00:F2}".PadLeft(valueWidth))
+                .AppendLine($"Zero-Rated Sales: {0.00:F2}".PadLeft(valueWidth))
+                .AppendLine($"VAT-Exempt Sales: {0.00:F2}".PadLeft(valueWidth))
+                .ToString();
+        }
+
+       
+    }
+}
